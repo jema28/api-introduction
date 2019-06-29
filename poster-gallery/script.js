45447418f46fec0c56200bbea56d909ea5ed3940
Jemila <jemila.abulhawa@gmail.com>
2019-06-29 21:34:33 +0300

    add gallery js solution

--- poster-gallery/script.js
+++ poster-gallery/script.js
@@ -1,1 +1,20 @@
-// Add your fetch here
+const posterContainer = document.getElementById('poster-gallery')
+
+fetch('http://www.omdbapi.com/?s=harry+potter&apikey=thewdb')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data['Search']
+    const moviesSortedByYear = movies.sort((a, b) => a['Year'] - b['Year'])
+    const filteredMovies = moviesSortedByYear.filter(movie => movie['Title'] !== "Harry Potter and the Forbidden Journey" && movie['Type'] !== "game")
+    const posterUrls = filteredMovies.map(movie => movie['Poster'])
+
+    return posterUrls.map(url => {
+      const poster = document.createElement('img')
+      poster.setAttribute("src", url)
+      poster
+        .classList
+        .add("poster")
+      posterContainer.appendChild(poster);
+    })
+  })
+  .catch(error => console.log(error))
